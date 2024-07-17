4dd4e67b5023325d6bc45d24e145c747ac43289f
V1ncenttt <vincentlfve@gmail.com>
2024-07-17 13:41:50 +0100

    Started implementing PINN

---
+++ src/pinn.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+import torch.autograd as autograd
+
+def physics_informed_loss(model, inputs, boundary_inputs, boundary_outputs, rho, mu, dt):
+    return None
+
+class PINN(nn.Module):
+    pass 
+
+
+
+if __name__ == '__main__':
+    net = PINN()
+    
